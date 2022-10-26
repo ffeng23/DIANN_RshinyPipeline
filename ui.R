42ad60ab55e6bf2f9ebee362c2c7adde3c21516e
Feng Feng <FFeng@senseibio.com>
2022-10-26 09:28:14 -0400

    minor changes. updating

--- ui.R
+++ ui.R
@@ -94,62 +94,7 @@ ui <- fluidPage(
                 #tableOutput("table")
                 h1("Will show some analysis here!!")
         )
-      ),
-   #section of file and report summary
-  fluidRow(
-    #column 1
-    column(4, 
-      "DIANN Report Summary",
-      numericInput("n1", label = "n", value = 1000, min = 1),
-      numericInput("mean1", label = "µ", value = 0, step = 0.1),
-      numericInput("sd1", label = "σ", value = 0.5, min = 0.1, step = 0.1)
-    ),
-        #column 2
-    column(4, 
-      "Distribution 2",
-      numericInput("n2", label = "n", value = 1000, min = 1),
-      numericInput("mean2", label = "µ", value = 0, step = 0.1),
-      numericInput("sd2", label = "σ", value = 0.5, min = 0.1, step = 0.1)
-    ),
-        #column 3
-    column(4,
-      "Frequency polygon",
-      numericInput("binwidth", label = "Bin width", value = 0.1, step = 0.1),
-      sliderInput("range", label = "range", value = c(-3, 3), min = -5, max = 5)
-    )
-  ),
-  fluidRow(
-    column(9, plotOutput("hist")),
-    column(3, verbatimTextOutput("ttest"))
-  ),
-  
-  #########
-  #another section with 
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot"),
-	  plotOutput(outputId = "ggPlot"),
-	  textOutput("text1")
-	  #textOutput("text2")
-
-    )
-  )
+      )
+   
   #end sidebarLayout
 )
